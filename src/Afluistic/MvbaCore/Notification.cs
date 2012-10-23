3966fe447509a8e16b978fb4c957235227062086
Clinton Sheppard <gar3ts@gmail.com>
2012-10-23 10:10:43 -0500

    update to latest MvbaCore/Notification.cs

--- src/Afluistic/MvbaCore/Notification.cs
+++ src/Afluistic/MvbaCore/Notification.cs
@@ -14,196 +14,205 @@ using System.Linq;
 
 namespace Afluistic.MvbaCore
 {
-	public class Notification<T> : Notification
-	{
-		public Notification()
-		{
-		}
-
-		public Notification(NotificationMessage notificationMessage)
-			: base(notificationMessage)
-		{
-		}
-
-		public Notification(Notification notification) : this(notification, default(T))
-		{
-		}
-
-		public Notification(Notification notification, T item)
-		{
-			Item = item;
-			Add(notification);
-		}
-
-		public new static Notification<T> Empty
-		{
-			get { return new Notification<T>(); }
-		}
-
-		public T Item { get; set; }
-
-		public static implicit operator T(Notification<T> notification)
-		{
-			if (notification.HasErrors)
-			{
-				throw new ArgumentNullException(string.Format("Cannot implicitly cast Notification<{0}> to {0} when there are errors.", typeof(T).Name));
-			}
-			return notification.Item;
-		}
-
-		public static implicit operator Notification<T>(T item)
-		{
-			return new Notification<T>
-				       {
-					       Item = item
-				       };
-		}
-	}
-
-	public abstract class NotificationBase
-	{
-		protected NotificationBase()
-		{
-			Messages = new List<NotificationMessage>();
-		}
-
-		protected NotificationBase(NotificationMessage notificationMessage)
-			: this()
-		{
-			Messages = new List<NotificationMessage>
-				           {
-					           notificationMessage
-				           };
-		}
-
-		public string Errors
-		{
-			get { return Messages.Where(x => x.Severity == NotificationSeverity.Error).Select(x => x.Message).Join(Environment.NewLine); }
-		}
-
-		public string ErrorsAndWarnings
-		{
-			get { return Messages.Where(x => x.Severity == NotificationSeverity.Error || x.Severity == NotificationSeverity.Warning).Select(x => x.Message).Join(Environment.NewLine); }
-		}
-
-        public bool HasErrors
+    public class Notification<T> : Notification
+    {
+        public Notification()
         {
-            get { return Messages.Any(x => x.Severity == NotificationSeverity.Error); }
         }
 
-        public bool HasWarnings
+        public Notification(NotificationMessage notificationMessage)
+            : base(notificationMessage)
         {
-            get { return Messages.Any(x => x.Severity == NotificationSeverity.Warning); }
         }
 
-		public string Infos
-		{
-			get { return Messages.Where(x => x.Severity == NotificationSeverity.Info).Select(x => x.Message).Join(Environment.NewLine); }
-		}
+        public Notification(Notification notification)
+            : this(notification, default(T))
+        {
+        }
 
-        [Obsolete("use HasErrors and/or HasWarnings")]
-		public bool IsValid
-		{
-			get { return !(HasErrors || HasWarnings); }
-		}
+        public Notification(Notification notification, T item)
+        {
+            Item = item;
+            Add(notification);
+        }
+
+        public new static Notification<T> Empty
+        {
+            get { return new Notification<T>(); }
+        }
+
+        public T Item { get; set; }
+
+        public static implicit operator T(Notification<T> notification)
+        {
+            if (notification.HasErrors)
+            {
+                throw new ArgumentNullException(string.Format("Cannot implicitly cast Notification<{0}> to {0} when there are errors.", typeof(T).Name));
+            }
+            return notification.Item;
+        }
+
+        public static implicit operator Notification<T>(T item)
+        {
+            return new Notification<T>
+            {
+                Item = item
+            };
+        }
+    }
+
+    public abstract class NotificationBase
+    {
+        protected NotificationBase()
+        {
+            Messages = new List<NotificationMessage>();
+        }
+
+        protected NotificationBase(NotificationMessage notificationMessage)
+            : this()
+        {
+            AddMessage(notificationMessage);
+        }
+
+        public string Errors
+        {
+            get { return !HasErrors ? "" : GetMessages(x => x.Severity == NotificationSeverity.Error); }
+        }
+
+        public string ErrorsAndWarnings
+        {
+            get { return !(HasErrors || HasWarnings) ? "" : GetMessages(x => x.Severity == NotificationSeverity.Error || x.Severity == NotificationSeverity.Warning); }
+        }
+
+        public bool HasErrors { get; private set; }
+
+        public bool HasWarnings { get; private set; }
+
+        public string Infos
+        {
+            get { return GetMessages(x => x.Severity == NotificationSeverity.Info); }
+        }
+
+        public bool IsValid
+        {
+            get { return !(HasErrors || HasWarnings); }
+        }
 
         public List<NotificationMessage> Messages { get; private set; }
-
-		public void Add(Notification notification)
-		{
-			foreach (var message in notification.Messages)
-			{
-				AddMessage(message);
-			}
-		}
-
-		public void Add(NotificationMessage message)
-		{
-			AddMessage(message);
-		}
-
-		private void AddMessage(NotificationMessage message)
-		{
-			if (!(Messages.Any(x => x.Severity == message.Severity && x.Message == message.Message)))
-			{
-				Messages.Add(message);
-			}
-		}
-
-		public override string ToString()
-		{
-            return ErrorsAndWarnings;
-		}
-
         public string Warnings
         {
-            get { return Messages.Where(x => x.Severity == NotificationSeverity.Warning).Select(x => x.Message).Join(Environment.NewLine); }
+            get { return !HasWarnings ? "" : GetMessages(x => x.Severity == NotificationSeverity.Warning); }
         }
-	}
 
-	public class Notification : NotificationBase
-	{
-		public Notification()
-		{
-		}
+        public void Add(Notification notification)
+        {
+            foreach (var message in notification.Messages)
+            {
+                AddMessage(message);
+            }
+        }
 
-		public Notification(NotificationMessage notificationMessage)
-			: base(notificationMessage)
-		{
-		}
+        public void Add(NotificationMessage message)
+        {
+            AddMessage(message);
+        }
 
-		public static Notification Empty
-		{
-			get { return new Notification(); }
-		}
+        private void AddMessage(NotificationMessage message)
+        {
+            if (!(Messages.Any(x => x.Severity == message.Severity && x.Message == message.Message)))
+            {
+                switch (message.Severity)
+                {
+                    case NotificationSeverity.Error:
+                        HasErrors = true;
+                        break;
+                    case NotificationSeverity.Warning:
+                        HasWarnings = true;
+                        break;
+                }
+                Messages.Add(message);
+            }
+        }
 
-		public static Notification ErrorFor(string messageText)
-		{
-			return For(NotificationSeverity.Error, messageText);
-		}
+        private string GetMessages(Func<NotificationMessage, bool> predicate)
+        {
+            return Messages.Where(predicate).Select(x => x.Message).Join(Environment.NewLine);
+        }
 
-		public static Notification ErrorFor(string messageFormatString, params object[] messageParameters)
-		{
-			return new Notification(new NotificationMessage(NotificationSeverity.Error, messageFormatString, messageParameters));
-		}
+        public override string ToString()
+        {
+            return ErrorsAndWarnings;
+        }
+    }
 
-		public static Notification For(NotificationSeverity severity, string messageText)
-		{
-			return new Notification(new NotificationMessage(severity, messageText));
-		}
+    public class Notification : NotificationBase
+    {
+        public Notification()
+        {
+        }
 
-		public static Notification InfoFor(string messageText)
-		{
-			return For(NotificationSeverity.Info, messageText);
-		}
+        public Notification(NotificationMessage notificationMessage)
+            : base(notificationMessage)
+        {
+        }
 
-		public static Notification InfoFor(string messageFormatString, params object[] messageParameters)
-		{
-			return new Notification(new NotificationMessage(NotificationSeverity.Info, messageFormatString, messageParameters));
-		}
+        public static Notification Empty
+        {
+            get { return new Notification(); }
+        }
 
-		public static Notification WarningFor(string messageText)
-		{
-			return For(NotificationSeverity.Warning, messageText);
-		}
+        public static Notification ErrorFor(string messageText)
+        {
+            return For(NotificationSeverity.Error, messageText);
+        }
 
-		public static Notification WarningFor(string messageFormatString, params object[] messageParameters)
-		{
-			return new Notification(new NotificationMessage(NotificationSeverity.Warning, messageFormatString, messageParameters));
-		}
-	}
+        public static Notification ErrorFor(string messageFormatString, params object[] messageParameters)
+        {
+            return For(NotificationSeverity.Error, messageFormatString, messageParameters);
+        }
 
-	public static class NotificationExtensions
-	{
-		public static Notification<T> ToNotification<T>(this Notification notification)
-		{
-			// becuase we can't implicitly cast up from a base class
-			return new Notification<T>(notification);
-		}
+        public static Notification For(NotificationSeverity severity, string messageText)
+        {
+            return new Notification(new NotificationMessage(severity, messageText));
+        }
 
-		public static Notification<T> ToNotification<T>(this Notification notification, T item)
-		{
-			return new Notification<T>(notification, item);
-		}
-	}
+        private static Notification For(NotificationSeverity severity, string messageFormatString, params object[] messageParameters)
+        {
+            return new Notification(new NotificationMessage(severity, messageFormatString, messageParameters));
+        }
+
+        public static Notification InfoFor(string messageText)
+        {
+            return For(NotificationSeverity.Info, messageText);
+        }
+
+        public static Notification InfoFor(string messageFormatString, params object[] messageParameters)
+        {
+            return For(NotificationSeverity.Info, messageFormatString, messageParameters);
+        }
+
+        public static Notification WarningFor(string messageText)
+        {
+            return For(NotificationSeverity.Warning, messageText);
+        }
+
+        public static Notification WarningFor(string messageFormatString, params object[] messageParameters)
+        {
+            return For(NotificationSeverity.Warning, messageFormatString, messageParameters);
+        }
+    }
+
+    public static class NotificationExtensions
+    {
+        public static Notification<T> ToNotification<T>(this Notification notification)
+        {
+            // because we can't implicitly cast up from a base class
+            return new Notification<T>(notification);
+        }
+
+        public static Notification<T> ToNotification<T>(this Notification notification, T item)
+        {
+            return new Notification<T>(notification, item);
+        }
+    }
 }
