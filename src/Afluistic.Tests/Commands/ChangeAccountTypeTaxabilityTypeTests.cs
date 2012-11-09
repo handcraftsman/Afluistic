20f5429a1dd51b9ba6381fe062100cb977d447d9
Clinton Sheppard <gar3ts@gmail.com>
2012-11-09 01:08:08 -0600

    add test for 'change account type taxability type' using an index

--- src/Afluistic.Tests/Commands/ChangeAccountTypeTaxabilityTypeTests.cs
+++ src/Afluistic.Tests/Commands/ChangeAccountTypeTaxabilityTypeTests.cs
@@ -54,7 +54,20 @@ namespace Afluistic.Tests.Commands
         public class When_asked_to_execute
         {
             [TestFixture]
-            public class Given_the_first_argument_is_not_an_existing_account_type : IntegrationTestBase
+            public class Given_the_first_argument_is_not_an_existing_account_type_index : IntegrationTestBase
+            {
+                [Test]
+                public void Should_return_the_correct_error_message()
+                {
+                    Subcutaneous.FromCommandline()
+                        .Init("x:")
+                        .ChangeAccountTypeTaxabilityType("0", TaxabilityType.Taxable.Key)
+                        .VerifyStandardErrorMatches(IsTheIndexOfAnExistingAccountType.IndexDoesNotExistMessageText);
+                }
+            }
+
+            [TestFixture]
+            public class Given_the_first_argument_is_not_an_existing_account_type_name : IntegrationTestBase
             {
                 [Test]
                 public void Should_return_the_correct_error_message()
